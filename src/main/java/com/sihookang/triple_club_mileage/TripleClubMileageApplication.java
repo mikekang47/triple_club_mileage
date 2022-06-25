55565e354afef2744840109299c7945c91104c00
mikekang47 <gyeong0308@gmail.com>
2022-06-25 20:52:37 +0900

    Set environment

---
+++ src/main/java/com/sihookang/triple_club_mileage/TripleClubMileageApplication.java
@@ -0,0 +1,13 @@
+package com.sihookang.triple_club_mileage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripleClubMileageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripleClubMileageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sihookang/triple_club_mileage/TripleClubMileageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sihookang.triple_club_mileage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripleClubMileageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
