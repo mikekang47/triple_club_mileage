fe3b7f4f6e786021b387de8b004926ce14383475
mikekang47 <gyeong0308@gmail.com>
2022-06-26 18:33:29 +0900

    Add: add contructor and getter/setter annotations in Place domain

--- src/main/java/com/sihookang/triple_club_mileage/domain/Place.java
+++ src/main/java/com/sihookang/triple_club_mileage/domain/Place.java
@@ -1,10 +1,19 @@
 package com.sihookang.triple_club_mileage.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @Entity
 @Table(name = "PLACE")
 public class Place {
